b640f3c4f45bc86881b7d3b04f5a35bb92f08251
Ekam Bitt <74407205+Ekam-Bitt@users.noreply.github.com>
2023-06-22 20:48:07 +0530

    added all files except node modules

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+function Square({ value, onClick }) {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    const newSquares = [...squares];
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  };
+
+  const winner = calculateWinner(squares);
+  const isDraw = squares.every((square) => square !== null);
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else if (isDraw) {
+    status = 'Draw';
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function App() {
+  const handleRestart = () => {
+    window.location.reload(); // Reload the page to reset the game
+  };
+
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <div className="board-container">
+        <Board />
+      </div>
+      <div className="restart-button-container">
+        <button className="restart-button" onClick={handleRestart}>
+          Restart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
